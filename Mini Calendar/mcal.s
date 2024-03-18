88a4902fbfc1e7d53ee0fd27d097aca6eec16c7d
Ryan Miller <miller.ryan2005@gmail.com>
2024-03-17 21:27:38 -0700

    Finished mini calendar

--- Mini Calendar/mcal.s
+++ Mini Calendar/mcal.s
@@ -27,9 +27,13 @@ a11:	.asciiz	"November "
 a12:	.asciiz	"December "
 months:	.word	a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12
 
+monthLengths:	.word	31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+
 outline:	.asciiz	"Sun Mon Tue Wed Thu Fri Sat\n"
 
-spaces:	.asciiz	"   "
+spaces3:	.asciiz	"   "
+spaces2:	.asciiz	"  "
+spaces1:	.asciiz	" "
 
 newLine:	.asciiz	"\n"
 
@@ -41,6 +45,9 @@ newLine:	.asciiz	"\n"
 # y (for days) - $t3
 # m (for days) - $t4
 # day of the week - $t5
+# temp reg - $t6
+# temp reg - $t7
+# 
 
 main:
 	la	$a0, intro		# print intro
@@ -91,6 +98,7 @@ main:
 	rem	$t5, $t5, 7		# everything % 7
 
 	la	$t6, months		# load months array address
+	sub	$t0, $t0, 1		# subtract months by 1 to turn it into array index
 	sll	$t7, $t0, 2		# multiply month index by 4 for address
 	addu	$t6, $t6, $t7		# get address for specific month
 	lw	$a0, ($t6)
@@ -109,12 +117,75 @@ main:
 	li	$v0, 4
 	syscall
 
-	while:	bge $t8, $t5, endw	# loop according to day of the week
-	la	$a0, spaces
+
+
+	li	$t6, 0			# use t6 for counter
+	while:	bge $t6, $t5, endw	# loop according to day of the week
+	la	$a0, spaces3		# print 3 spaces for empty days
 	li	$v0, 4
-	
+	syscall
+
+	la	$a0, spaces1		# space between every day
+	li	$v0, 4
+	syscall
+
+	addi	$t6, $t6, 1
+	b while
 	endw:
 
+	la	$t6, monthLengths
+	sll	$t7, $t0, 2
+	addu	$t6, $t6, $t7
+	lw	$t8, ($t6)		# put total number of days in the month in $t8
+
+	if3:	bne $t0, 1, endif3	# if month is february
+	
+	and	$t7, $t1, 0x3
+	if4:	bne $t7, $zero, endif4	# if year is divisble by 4
+
+	rem	$t7, $t1, 100
+	rem	$t9, $t1, 400
+	add	$t7, $t7, $t9
+	if5:	beq $t7, $zero, endif5	# if year is divisble by 100 or 400
+	
+	addi	$t8, $t8, 1		# add 1 to februrary days if not divisble by 100 or 400
+	
+	endif5:
+	endif4:
+	endif3:
+
+	li	$t6, 1			# reset t6 counter
+	while2:	bgt $t6, $t8, endw2	# loop the days in the month
+
+	rem	$t7, $t5, 7
+	if:	bne $t7, $zero, endif	# if day of the week is sunday, print new line before printing the day
+	la	$a0, newLine		
+	li	$v0, 4
+	syscall
+	endif:
+
+	if2:	bge $t6, 10, endif2
+	la	$a0, spaces1		# print an extra space for single digit numbers
+	li	$v0, 4
+	syscall
+	
+	endif2:
+	la	$a0, spaces1		# print at least one space for all numbers
+	li	$v0, 4
+	syscall
+
+	move	$a0, $t6		# print day of the week
+	li	$v0, 1
+	syscall
+
+	la	$a0, spaces1		# print space between each day
+	li	$v0, 4
+	syscall
+
+	addi	$t5, $t5, 1		# increase day of the week
+	addi	$t6, $t6, 1		# increase day
+	b while2
+	endw2:
 
 	la	$a0, newLine		# print new line
 	li	$v0, 4
